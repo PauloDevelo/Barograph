e78449e68b81b375e66293a0d3120e88bbbcfd86
Paul TORRUELLA <paul.torruella@gmail.com>
2016-03-28 12:38:28 +1300

    Update barograph.ino

--- src/barograph.ino
+++ src/barograph.ino
@@ -16,12 +16,8 @@
   http://www.ecogium.fr/arbutus/contact.php.
 
   This library is free software; you can redistribute it and/or
-  modify it under the terms of the CC BY-NC-SA 3.0 license.
+  modify it under the terms of the GPL 3.0 license.
   Please see the included documents for further information.
-
-  Commercial use of this program requires you to buy a license that
-  will allow commercial use. This includes using the code,
-  modified or not, as a tool to sell products.
 
   The license applies to barograph.ino and the documentation.
 */
